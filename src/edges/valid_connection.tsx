0227c470cc7b429ab548a64d7d769058234dfcab
xinxia <starysky.yaoding@icloud.com>
2025-08-24 02:23:02 +0800

    [fix] 修复content连接问题

--- src/edges/valid_connection.tsx
+++ src/edges/valid_connection.tsx
@@ -6,16 +6,29 @@ const validMap: Record<string, string[]> = {
     if: ["ifstmt", "else"],
     ifstmt: ["content", "choice", "if"],
     else: ["content", "choice", "if"],
-    content: ["*"],
+    content: ["-", 'case'],
     case: ["event"],
 }
 
 export function IsValidFlowConnection(connection: Connection | Edge): boolean {
     for (const [key, values] of Object.entries(validMap)) {
         if (connection.source.startsWith(`${key}-`)) {
+            // * 表示所有节点都可以连接
             if (values.length == 1 && values[0] == "*") {
                 return true;
             }
+
+            // 黑名单模式
+            if (values.length > 0 && values[0] == "-") {
+                for (const value of values) {
+                    if (connection.target.startsWith(`${value}-`)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            // 白名单模式
             for (const value of values) {
                 if (connection.target.startsWith(`${value}-`)) {
                     return true;
